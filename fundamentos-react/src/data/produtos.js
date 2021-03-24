ffe893395def6086d5d29d6cac7cbbb9dbf5b7d3
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-03-24 08:24:07 -0300

    data

---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,14 @@
+const produtos = [
+  {id:1, nome: 'Notebook', preco: 3000},
+  {id:2, nome: 'Nokia', preco: 3000},
+  {id:3, nome: 'Iphone', preco: 3000},
+  {id:4, nome: 'Calça', preco: 3000},
+  {id:5, nome: 'Teclado', preco: 3000},
+  {id:6, nome: 'Monitor', preco: 3000},
+  {id:7, nome: 'Shampoo', preco: 3000},
+  {id:8, nome: 'Maquiagem', preco: 3000},
+  {id:9, nome: 'Pulseira', preco: 3000},
+];
+
+
+export default produtos;
