f2fc2fcea116e8b03b920a57f027e4395c97f14d
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-05-04 14:58:17 -0300

    reducer for user, made

---
+++ hook/src/reducers/user.js
@@ -0,0 +1,12 @@
+
+
+export  function userReducer(state, action) {
+
+  switch (action.type) {
+      case 'login':
+          return { ...state, user: { name: action.payload } }
+    
+      default:
+          return state
+  }
+}
