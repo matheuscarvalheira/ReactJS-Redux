8c708ebeeb0adb3e0e544525d69fb3dcf80675c0
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-05-04 14:54:52 -0300

    exporting reducers and the initial state

---
+++ hook/src/store/index.js
@@ -0,0 +1,18 @@
+
+import {reducer} from '../reducers';
+
+
+
+
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  //foco no numero
+  number: 0
+}
+
+export {
+  reducer,
+  initialState
+}
