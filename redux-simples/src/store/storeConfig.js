c894b961044a5857a63897abb807f1dd834eaccf
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-05-20 21:06:19 -0300

    general state of application

---
+++ redux-simples/src/store/storeConfig.js
@@ -0,0 +1,29 @@
+import {createStore, combineReducers} from 'redux';
+
+
+const reducers = combineReducers({
+  numeros:  function(state,action){
+    console.log(state, '', action)
+    return{
+      min: 7,
+      max: 31
+    }
+  },
+  nomes: function(state,action){
+    console.log(state, '', action)
+    return[
+      'Ana',
+      'Bia',
+      'Carlos'
+    ]
+      
+    
+  }
+})
+
+function storeConfig(){
+  //criar meu estado global
+  return createStore(reducers)
+}
+
+export default storeConfig;
