45b4e3c589f350d837e2509984c5413e1fd680d4
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2021-05-20 21:07:21 -0300

    integration of redux with reactJS

--- redux-simples/src/index.js
+++ redux-simples/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'; 
+import configStore from './store/storeConfig';
+
+//estado formado a partir do createStore(reducers)
+const store = configStore(); 
+
 ReactDOM.render(
+  <Provider store= {store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
